35bebc2a822c82ca02704eb3c14f7d035a2a1c8d
Max Eddy <maxiscool@gmail.com>
2014-11-14 09:30:34 -0500

    added some more recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,14 @@
-include_recipe 'apache2'
 include_recipe 'php'
+include_recipe 'apache2'
+include_recipe "apache2::mod_rewrite"
+include_recipe "apache2::mod_php5"
+
+package 'php5-mysql' do
+  action :install
+end
+
+include_recipe 'mysql::server'
 include_recipe 'nodejs'
 include_recipe 'git'
 include_recipe 'vim'
-include_recipe 'mysql::server'
 include_recipe 'tmux'
